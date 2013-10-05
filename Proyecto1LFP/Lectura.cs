96899b9b4175d2fe1afd441f5003938fc540d712
Hugo Allan Garcia Monterrosa <hugoallangm@gmail.com>
2013-10-05 10:06:46 -0600

    Inicio del Proyecto

---
+++ Proyecto1LFP/Lectura.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.IO;
+
+namespace Proyecto1LFP
+{
+    class Lectura
+    {
+        private ArrayList linea = new ArrayList();
+        private String lineaLeida = "";
+        private String ruta;
+
+        public void setRuta(String ruta)
+        {
+            this.ruta = ruta;
+        }
+
+        public void leer()
+        {
+            StreamReader lector = new StreamReader(ruta);
+            while (lineaLeida != null)
+            {
+                lineaLeida = lector.ReadLine();
+                if (lineaLeida != null)
+                    linea.Add(lineaLeida);
+            }
+            lector.Close();
+
+        }
+
+        public ArrayList getArray()
+        {
+            return linea;
+        }
+    }
+}
